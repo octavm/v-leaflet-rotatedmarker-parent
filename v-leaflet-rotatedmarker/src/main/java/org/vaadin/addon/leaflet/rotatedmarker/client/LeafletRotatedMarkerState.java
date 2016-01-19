12dd6b41dbdb6900eb9ddeeaf83fa855071c5309
octavm <octavm@startmail.com>
2016-01-19 12:28:34 +0100

    Create LeafletRotatedMarkerState.java

---
+++ v-leaflet-rotatedmarker/src/main/java/org/vaadin/addon/leaflet/rotatedmarker/client/LeafletRotatedMarkerState.java
@@ -0,0 +1,10 @@
+package org.vaadin.addon.leaflet.rotatedmarker.client;
+
+import org.vaadin.addon.leaflet.shared.LeafletMarkerState;
+
+public class LeafletRotatedMarkerState extends LeafletMarkerState {
+
+    public Double rotationAngle;
+    public String rotationOrigin;
+
+}
