a1c1c5c8e46cdbb5eb107d9d299fa6b021bdb527
octavm <octavm@startmail.com>
2016-01-19 12:21:33 +0100

    Create RotatedMarker.java

---
+++ g-leaflet-rotatedmarker/src/main/java/org/vaadin/gleaflet/rotatedmarker/client/RotatedMarker.java
@@ -0,0 +1,34 @@
+package org.vaadin.gleaflet.rotatedmarker.client;
+
+import org.peimari.gleaflet.client.*;
+import org.vaadin.gleaflet.rotatedmarker.client.resources.LeafletRotatedMarkerResourceInjector;
+
+/**
+ * 
+ * @author octavm
+ *
+ */
+public class RotatedMarker extends Marker {
+	
+	static {
+		LeafletRotatedMarkerResourceInjector.ensureInjected();
+	}
+	
+	protected RotatedMarker() {}
+
+	public static native RotatedMarker create(LatLng latlng, MarkerOptions options)
+	/*-{
+		return new $wnd.L.Marker(latlng, options);
+	}-*/;
+	
+	public native final void setRotationAngle(double newAngle)
+	/*-{
+		this.setRotationAngle(newAngle);
+	}-*/;
+
+    public native final void setRotationOrigin(String newOrigin)
+    /*-{
+        this.setRotationOrigin(newOrigin);
+    }-*/;
+
+}
